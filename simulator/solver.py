307b3371a1230ccc8bdc5f30266f961c19eb3851
Prageeth Jayathissa <p.jayathissa@gmail.com>
2016-04-07 21:31:58 +0200

    Fixed lighting error, added radiation data

--- simulator/solver.py
+++ simulator/solver.py
@@ -50,13 +50,18 @@ Ri=42 #[K/kWh] Resistance to outside air. Based off glass having a Uvalue of 1.9
 Q_fenstRad=Q_fenstRad*glass_solar_transmitance #Solar Gains
 Q=Q_fenstRad.add(Q_human, axis=0) #only solar gains and human gains for now
 
+#Initialise Conditions
 Q_heat=0
 Q_cool=0
-Total_Heat=0
+Total_Heating=0
 Total_Cooling=0
 Total_Lighting=0
 Ti=20 #Starting internal temperature
 Data_Ti=[] 
+Data_Heating=np.empty([0,8760])
+print Data_Heating
+Data_Cooling=[]
+Data_Lighting=[]
 
 #PID setup
 heatingControl=PID_controller.PID(P=2.0, I=4.0, D=0.0, Derivator=0, Integrator=0, Integrator_max=500, Integrator_min=-500)
@@ -66,8 +71,14 @@ coolingControl=PID_controller.PID(P=2.0, I=4.0, D=0.0, Derivator=0, Integrator=0
 dt=.25 #hours
 
 for ii in range(0, int(8760)):
+	#Initialise hourly energy requirements
+	Heat_hr=0
+	Cool_hr=0
+
+
 	for jj in range(0,int(1/dt)):
-	
+
+			
 
 		dTi=((Q.iat[ii,0]+Q_heat+Q_cool)/(Cm) + (1/(Cm*Ri))*(float(To[ii])-Ti))*dt
 		Ti=Ti+dTi
@@ -77,27 +88,32 @@ for ii in range(0, int(8760)):
 			Q_heat= heatingControl.update(Ti)/10
 		else:
 			Q_heat=0
-		Total_Heat+=Q_heat
+		Heat_hr+=Q_heat
+
 
 		if Ti>tintC_set:
 			coolingControl.setPoint(tintC_set)
 			Q_cool = coolingControl.update(Ti)/2
 		else:
 			Q_cool=0
-		Total_Cooling+=Q_cool
+		Cool_hr+=Q_cool
 
-		#Lighting calc. Double check this as I wrote it rushed. Try find allternative for .iat
 
-		if Lux.get_value(ii,0)< LightingControl and occupancy['People'].iat[ii]>0:
-			Total_Lighting+=LightLoad*Floor_A
+	Total_Heating+=Heat_hr
+	Total_Cooling+=Cool_hr
+	#Lighting calc. Double check this as I wrote it rushed. Try find allternative for .iat
+
+	if Lux.get_value(ii,0)< LightingControl and occupancy['People'].iat[ii]>0:
+		Total_Lighting+=LightLoad*Floor_A
+
 
 
 	Data_Ti.append(Ti)
 
-print 'Total Heating Load is:', Total_Heat, 'kWh'
+print 'Total Heating Load is:', Total_Heating, 'kWh'
 print 'Total Cooling Load is:', Total_Cooling*-1, 'kWh'
 print 'Total Lighting Load is:', Total_Lighting, 'kWh'
-#These values are still a little high, but in the right magnitude at least
+#Heating and Lighting values look good. Cooling is about 2x high. Most likely because theres no natural ventilation
 
 
 # plt.plot(range(0, int(8760)),To, range(0, int(8760)),Data_Ti)
