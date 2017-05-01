730e33eed57e5de436d5567c97957bfa401852f5
Prageeth Jayathissa <p.jayathissa@gmail.com>
2017-05-01 18:25:37 +0200

    typo in example fixed

--- 5R1C_ISO_simulator/example.py
+++ 5R1C_ISO_simulator/example.py
@@ -25,7 +25,7 @@ theta_e=10
 theta_m_prev=22
 phi_int=10 #Internal heat gains, in Watts
 phi_sol=2000 #Solar heat gains after transmitting through the winow [Watts]
-ill=44000 #Illuminance after transmitting through the window [Lumens]
+ill=44000 #Illuminance after before transmitting through the window [Lumens]
 occupancy = 0.1 #Occupancy for the timestep [people/hour/square_meter]
 
 
