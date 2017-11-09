af9272b87feef9261ab863e9507ee606152e140e
Canhejust <zarb.uno@gmail.com>
2017-11-09 16:20:22 +0100

    schedules can be read from csv but theres something wrong with the math. need to check units.

---
+++ rc_simulator/grasshopper_component/calc_occupancy.py
@@ -0,0 +1,4 @@
+﻿# This doesn't make sense yet 
+#The units for rate should be 
+# occupancy = rate * number of occupants * area
+occupancy = list(map(lambda x:area*number_of_occupants*x, occupancy_per_m2))
