729225dd66e866935512921788b1eb74eb43b585
pjayathissa <p.jayathissa@gmail.com>
2019-12-25 00:18:53 +0100

    started rough greenhouse calcs

--- rc_simulator/examples/annualSimulation.py
+++ rc_simulator/examples/annualSimulation.py
@@ -44,48 +44,45 @@ COP = []
 
 # Initialise the Location with a weather file
 Zurich = Location(epwfile_path=os.path.join(
-    mainPath, 'auxiliary', 'Zurich-Kloten_2013.epw'))
+		mainPath, 'auxiliary', 'wellington_2006.epw'))
 
 # Initialise an instance of the building. Empty spaces take on the default
 # parameters. See buildingPhysics.py to see the default values
-Office = Building(window_area=4.0,
-                  external_envelope_area=15.0,
-                  room_depth=7.0,
-                  room_width=5.0,
-                  room_height=3.0,
-                  lighting_load=11.7,
-                  lighting_control=300.0,
-                  lighting_utilisation_factor=0.45,
-                  lighting_maintenance_factor=0.9,
-                  u_walls=0.2,
-                  u_windows=1.1,
-                  ach_vent=1.5,
-                  ach_infl=0.5,
-                  ventilation_efficiency=0.6,
-                  thermal_capacitance_per_floor_area=165000,
-                  t_set_heating=20.0,
-                  t_set_cooling=26.0,
-                  max_cooling_energy_per_floor_area=-np.inf,
-                  max_heating_energy_per_floor_area=np.inf,
-                  heating_supply_system=supply_system.OilBoilerMed,
-                  cooling_supply_system=supply_system.HeatPumpAir,
-                  heating_emission_system=emission_system.NewRadiators,
-                  cooling_emission_system=emission_system.AirConditioning,)
+Greenhouse = Building(window_area=110.0,
+									external_envelope_area=110.0,
+									room_depth=10.0,
+									room_width=5.0,
+									room_height=3.0,
+									lighting_load=0.0,
+									lighting_control=0.0,
+									lighting_utilisation_factor=0.45,
+									lighting_maintenance_factor=0.9,
+									u_walls=0.2,
+									u_windows=5.0,
+									ach_vent=2.0,
+									ach_infl=1.2,
+									ventilation_efficiency=0.0,
+									thermal_capacitance_per_floor_area=20000,
+									t_set_heating=-20.0,
+									t_set_cooling=260.0,
+									max_cooling_energy_per_floor_area=-np.inf,
+									max_heating_energy_per_floor_area=np.inf,
+									heating_supply_system=supply_system.DirectHeater,
+									cooling_supply_system=supply_system.DirectCooler,
+									heating_emission_system=emission_system.NewRadiators,
+									cooling_emission_system=emission_system.AirConditioning,)
 
 # Define Windows
-SouthWindow = Window(azimuth_tilt=0, alititude_tilt=90, glass_solar_transmittance=0.7,
-                     glass_light_transmittance=0.8, area=4)
+SouthWindow = Window(azimuth_tilt=0, alititude_tilt=90, glass_solar_transmittance=0.5,
+										 glass_light_transmittance=0.8, area=30)
+
+NorthWindow = Window(azimuth_tilt=180, alititude_tilt=90, glass_solar_transmittance=0.5,
+										 glass_light_transmittance=0.8, area=30)
+
+Roof = Window(azimuth_tilt=180, alititude_tilt=0, glass_solar_transmittance=0.5,
+										 glass_light_transmittance=0.8, area=50)
 
 
-# Define constants for the building
-gain_per_person = 100  # W per person
-appliance_gains = 14  # W per sqm
-max_occupancy = 3.0
-
-
-# Read Occupancy Profile
-occupancyProfile = pd.read_csv(os.path.join(
-    mainPath, 'auxiliary', 'schedules_el_OFFICE.csv'))
 
 # Starting temperature of the builidng
 t_m_prev = 20
@@ -93,58 +90,50 @@ t_m_prev = 20
 # Loop through all 8760 hours of the year
 for hour in range(8760):
 
-    # Occupancy for the time step
-    occupancy = occupancyProfile.loc[hour, 'People'] * max_occupancy
-    # Gains from occupancy and appliances
-    internal_gains = occupancy * gain_per_person + \
-        appliance_gains * Office.floor_area
 
-    # Extract the outdoor temperature in Zurich for that hour
-    t_out = Zurich.weather_data['drybulb_C'][hour]
+		# Extract the outdoor temperature in Zurich for that hour
+		t_out = Zurich.weather_data['drybulb_C'][hour]
 
-    Altitude, Azimuth = Zurich.calc_sun_position(
-        latitude_deg=47.480, longitude_deg=8.536, year=2015, hoy=hour)
+		Altitude, Azimuth = Zurich.calc_sun_position(
+				latitude_deg=47.480, longitude_deg=8.536, year=2015, hoy=hour)
 
-    SouthWindow.calc_solar_gains(sun_altitude=Altitude, sun_azimuth=Azimuth,
-                                 normal_direct_radiation=Zurich.weather_data[
-                                     'dirnorrad_Whm2'][hour],
-                                 horizontal_diffuse_radiation=Zurich.weather_data['difhorrad_Whm2'][hour])
+		# Loop through all windows
+		for selected_window in [SouthWindow, NorthWindow, Roof]:
+			selected_window.calc_solar_gains(sun_altitude=Altitude, sun_azimuth=Azimuth,
+																			 normal_direct_radiation=Zurich.weather_data[
+																					 'dirnorrad_Whm2'][hour],
+																			 horizontal_diffuse_radiation=Zurich.weather_data['difhorrad_Whm2'][hour])
 
-    SouthWindow.calc_illuminance(sun_altitude=Altitude, sun_azimuth=Azimuth,
-                                 normal_direct_illuminance=Zurich.weather_data[
-                                     'dirnorillum_lux'][hour],
-                                 horizontal_diffuse_illuminance=Zurich.weather_data['difhorillum_lux'][hour])
 
-    Office.solve_building_energy(internal_gains=internal_gains,
-                                 solar_gains=SouthWindow.solar_gains, t_out=t_out, t_m_prev=t_m_prev)
 
-    Office.solve_building_lighting(
-        illuminance=SouthWindow.transmitted_illuminance, occupancy=occupancy)
+		Greenhouse.solve_building_energy(internal_gains=0,
+																 solar_gains=SouthWindow.solar_gains, t_out=t_out, t_m_prev=t_m_prev)
 
-    # Set the previous temperature for the next time step
-    t_m_prev = Office.t_m_next
 
-    HeatingDemand.append(Office.heating_demand)
-    HeatingEnergy.append(Office.heating_energy)
-    CoolingDemand.append(Office.cooling_demand)
-    CoolingEnergy.append(Office.cooling_energy)
-    ElectricityOut.append(Office.electricity_out)
-    IndoorAir.append(Office.t_air)
-    OutsideTemp.append(t_out)
-    SolarGains.append(SouthWindow.solar_gains)
-    COP.append(Office.cop)
+		# Set the previous temperature for the next time step
+		t_m_prev = Greenhouse.t_m_next
+
+		HeatingDemand.append(Greenhouse.heating_demand)
+		HeatingEnergy.append(Greenhouse.heating_energy)
+		CoolingDemand.append(Greenhouse.cooling_demand)
+		CoolingEnergy.append(Greenhouse.cooling_energy)
+		ElectricityOut.append(Greenhouse.electricity_out)
+		IndoorAir.append(Greenhouse.t_air)
+		OutsideTemp.append(t_out)
+		SolarGains.append(SouthWindow.solar_gains)
+		COP.append(Greenhouse.cop)
 
 annualResults = pd.DataFrame({
-    'HeatingDemand': HeatingDemand,
-    'HeatingEnergy': HeatingEnergy,
-    'CoolingDemand': CoolingDemand,
-    'CoolingEnergy': CoolingEnergy,
-    'IndoorAir': IndoorAir,
-    'OutsideTemp':  OutsideTemp,
-    'SolarGains': SolarGains,
-    'COP': COP
+		'HeatingDemand': HeatingDemand,
+		'HeatingEnergy': HeatingEnergy,
+		'CoolingDemand': CoolingDemand,
+		'CoolingEnergy': CoolingEnergy,
+		'IndoorAir': IndoorAir,
+		'OutsideTemp':  OutsideTemp,
+		'SolarGains': SolarGains,
+		'COP': COP
 })
 
 # Plotting has been commented out as it can not be conducted in a virtual environment over ssh
-annualResults[['HeatingEnergy', 'CoolingEnergy']].plot()
+annualResults[['OutsideTemp','IndoorAir']].plot(alpha=0.5)
 plt.show()
